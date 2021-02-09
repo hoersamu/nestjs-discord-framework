e683ce66275e952113850e99c19854c9b3909bf3
hoersamu <samuelhoera@gmail.com>
2021-02-09 13:34:55 +0100

    feat: update parameter names of the decorators

--- src/bot.decorators.ts
+++ src/bot.decorators.ts
@@ -5,6 +5,7 @@ import { BOT_CLIENT, BOT_COMMAND, BOT_EVENT, BOT_INSTANCE } from './constants';
 export const InjectBotClient = () => Inject(BOT_CLIENT);
 export const InjectBotInstance = () => Inject(BOT_INSTANCE);
 
-export const BotCommand = (name: string) => SetMetadata(BOT_COMMAND, name);
-export const BotEvent = (name: keyof ClientEvents) =>
-  SetMetadata(BOT_EVENT, name);
+export const BotCommand = (command: string) =>
+  SetMetadata(BOT_COMMAND, command);
+export const BotEvent = (event: keyof ClientEvents) =>
+  SetMetadata(BOT_EVENT, event);
